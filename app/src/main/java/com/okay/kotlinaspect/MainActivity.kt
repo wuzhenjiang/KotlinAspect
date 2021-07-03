1841c06dc13088ac2301ed49f138a48130db7b0b
wuzhenjiang <wuzhenjiang@okay.cn>
2021-07-03 10:30:56 +0800

    添加辅助工具

--- app/src/main/java/com/okay/kotlinaspect/MainActivity.kt
+++ app/src/main/java/com/okay/kotlinaspect/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
